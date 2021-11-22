c1d4e7f8de5b9f1152a62cf57200ab112d089964
Aryan <aryancodename47@gmail.com>
2021-11-22 19:38:20 +0530

    Base Commit with basic structure

---
+++ components/Nav.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Nav = () => {
+  return <div>Navbar here</div>;
+};
+
+export default Nav;
---
+++ pages/team.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const team = () => {
+  return (
+    <div>
+      <h1>This is teams page</h1>
+    </div>
+  );
+};
+
+export default team;
