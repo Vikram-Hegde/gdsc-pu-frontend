46264325c592b1bd64131470e01a25461d3a08ca
Vikram-Hegde <vikramhegde1440@gmail.com>
2021-11-26 13:22:16 +0530

    Added Logo placeholder

--- components/Layout/Footer/index.js
+++ components/Layout/Footer/index.js
@@ -1,11 +1,15 @@
 import React from "react";
+import styles from './style.module.css';
 
 const Footer = () => {
-  return (
-    <div>
-      <h1>This is footer</h1>
-    </div>
-  );
+	return (
+		<footer className={styles.footer}>
+      <div className={styles.footer__logo}>
+       LOGO SVG
+      </div>
+      <hr className={styles.footer__line}/>
+		</footer>
+	);
 };
 
 export default Footer;
