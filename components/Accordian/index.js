5689eb5a84d775e42a0d88729db6284b4276e280
Vikram-Hegde <vikramhegde1440@gmail.com>
2021-11-28 18:27:55 +0530

    FAQ section ✅

---
+++ components/Accordian/index.js
@@ -0,0 +1,53 @@
+import styles from "./style.module.css";
+import { useState } from "react";
+
+const qa = [
+	{
+		question: "Lorem ipsum dolor emet",
+		answer:
+			"lorem ipsum dolor emet, lorem dolor emet ipsum. Dolor emet ipsum emet",
+	},
+	{
+		question: "Lorem ipsum dolor emet",
+		answer:
+			"lorem ipsum dolor emet, lorem dolor emet ipsum. Dolor emet ipsum emet",
+	},
+	{
+		question: "Lorem ipsum dolor emet",
+		answer:
+			"lorem ipsum dolor emet, lorem dolor emet ipsum. Dolor emet ipsum emet",
+	},
+	{
+		question: "Lorem ipsum dolor emet",
+		answer:
+			"lorem ipsum dolor emet, lorem dolor emet ipsum. Dolor emet ipsum emet",
+	},
+];
+
+export default function Accordian() {
+	const [visible, toggleVisible] = useState(0);
+
+	return (
+		<div className="accordianGroup">
+			{qa.map(({ question, answer }, index) => {
+				return (
+					<div
+						className={styles.accordianItem}
+						key={index}
+						onClick={() => toggleVisible(index)}
+					>
+						<div className={styles.accordianQuestionWrapper}>
+							<div className={styles.accordianQuestion}>{question}</div>
+							<div className={styles.accordianIndicator}>
+								{visible === index ? "-" : "+"}
+							</div>
+						</div>
+						{visible === index ? (
+							<div className={styles.accordianAnswer}>{answer}</div>
+						) : null}
+					</div>
+				);
+			})}
+		</div>
+	);
+}
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,35 @@
 import PageWrapper from "../components/Layout/PageWrapper";
 import Button from "../components/globals/Button";
+import Accordian from "../components/Accordian";
+import Image from "next/image";
+import styles from "./style.module.css";
 export default function Home() {
-  return (
-    <PageWrapper
-      title="Home GDSC-PU"
-      description="Homepage GDSC presidency university"
-    >
-      <h1>HomePage</h1>
-      {/* Button Example */}
-      <Button
-        variant="primary"
-        // disabled="true"
-        text="Click Here"
-        type="submit"
-        onclick={() => console.log("Button Clicked")}
-      />
-    </PageWrapper>
-  );
+	return (
+		<PageWrapper
+			title="Home GDSC-PU"
+			description="Homepage GDSC presidency university"
+		>
+			<h1>HomePage</h1>
+			{/* Button Example */}
+			<Button
+				variant="primary"
+				// disabled="true"
+				text="Click Here"
+				type="submit"
+				onclick={() => console.log("Button Clicked")}
+			/>
+			<section className={styles.faq}>
+				<div className={styles.faqTitle}>FAQ</div>
+				<div className={styles.faqWrapper}>
+					<Image
+						src="/assets/illustrations/settings-2.png"
+						layout="responsive"
+						width={523}
+						height={392}
+					/>
+					<Accordian />
+				</div>
+			</section>
+		</PageWrapper>
+	);
 }
