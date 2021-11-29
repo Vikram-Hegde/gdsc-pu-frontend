231bedd813df4ef5d56af7f9f5c3193f18f720ae
Vikram-Hegde <vikramhegde1440@gmail.com>
2021-11-29 14:44:30 +0530

    minor bug fixes in css

--- pages/index.js
+++ pages/index.js
@@ -21,12 +21,14 @@ export default function Home() {
 			<section className={styles.faq}>
 				<div className={styles.faqTitle}>FAQ</div>
 				<div className={styles.faqWrapper}>
-					<Image
-						src="/assets/illustrations/settings-2.png"
-						layout="responsive"
-						width={523}
-						height={392}
-					/>
+					<div className={ styles.faqImage }>
+						<Image
+							src="/assets/illustrations/settings-2.png"
+							layout="responsive"
+							width={523}
+							height={392}
+						/>
+					</div>
 					<Accordian />
 				</div>
 			</section>
