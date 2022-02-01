016fecbe10788baf546503d87f2ef4252222abe0
Vikram-Hegde <vikramhegde1440@gmail.com>
2022-02-01 17:56:53 +0530

    make tabs component reusable

---
+++ pages/team/Tabs/memberData.js
@@ -0,0 +1,124 @@
+export const members = [
+	{
+		name: 'Trehan Sangpriya',
+		dept: 'Management',
+		pos: 'GDSC Lead',
+		img: 'https://firebasestorage.googleapis.com/v0/b/doco-img.appspot.com/o/image%2FGroup%2013-compressed.jpg?alt=media&token=ff66c499-5d05-483d-ac37-66b68284f71d',
+	},
+	{
+		name: 'Leander Nathan',
+		dept: 'Management',
+		pos: 'Lead',
+		img: 'https://firebasestorage.googleapis.com/v0/b/doco-img.appspot.com/o/image%2FMember%20%3D%20Leander.png?alt=media&token=680ba691-38c4-4fbf-97ea-13594994f534',
+	},
+	{
+		name: 'Himanshu Tiwari',
+		dept: 'Management',
+		pos: 'Co-Lead',
+		img: 'https://drive.google.com/uc?id=1XWQEsgPM8fDuW6POC_FM7xt0MnOYLtrH',
+	},
+	{
+		name: 'Aryan Bhandari',
+		dept: 'Tech',
+		pos: 'Lead',
+		img: 'https://firebasestorage.googleapis.com/v0/b/doco-img.appspot.com/o/image%2Fprofile-pic%20-%20Aryan%20Bhandari.jpg?alt=media&token=2d0a9ea7-4cc2-40d1-913b-6d52fdbfde8f',
+	},
+	{
+		name: 'Md. Imaad Thouheed',
+		dept: 'Tech',
+		pos: 'Web Developer',
+		img: 'https://drive.google.com/uc?id=1sIhTnPxgnm0iYyt18V9jVyQUHWvqaZEN',
+	},
+	{
+		name: 'Vikram',
+		dept: 'Tech',
+		pos: 'Web Developer',
+		img: 'https://drive.google.com/uc?id=1_R6fr1Dz8L-7H0ZaMrYeBWf2-tigVa1c',
+	},
+	{
+		name: 'Adhvaidh G S',
+		dept: 'Tech',
+		pos: 'Web Developer',
+		img: 'https://drive.google.com/uc?id=1SWHBmkaBKPDST6w_bZfzxHmvGdDwYHn5',
+	},
+	{
+		name: 'Himanshu Kumar',
+		dept: 'Tech',
+		pos: 'UI/UX Designer',
+		img: 'https://drive.google.com/uc?id=1Ojbm6xQNsYnQA0M3XyTdP5vHJtBp8SIp',
+	},
+	{
+		name: 'N Mohammed Adil',
+		dept: 'Tech',
+		pos: 'AI/ML Developer',
+		img: 'https://drive.google.com/uc?id=1EHxAvkSqsfEvc8ZBD599JgBI_3JPT5_H',
+	},
+	{
+		name: 'Kavya Sharma',
+		dept: 'Events',
+		pos: 'Lead',
+		img: 'https://firebasestorage.googleapis.com/v0/b/doco-img.appspot.com/o/image%2FMember%3DKavya.png?alt=media&token=178de665-bf64-4160-95e3-e79904753d3f',
+	},
+	{
+		name: 'Abhishek A',
+		dept: 'Events',
+		pos: 'Operations',
+		img: 'https://drive.google.com/uc?id=1dpuk6sl_Y_qpge0KjmRPv4uLL7Ne3lX4',
+	},
+	{
+		name: 'Neeha Fathima',
+		dept: 'Social Media',
+		pos: 'Lead',
+		img: 'https://firebasestorage.googleapis.com/v0/b/doco-img.appspot.com/o/image%2Fprofile1.neeha%20-%20Neeha%20Fathima.png?alt=media&token=c7737f5b-4ef9-4d36-8be8-6d3b89c1ce96',
+	},
+	{
+		name: 'Shristi Singh',
+		dept: 'Social Media',
+		pos: 'Designer',
+		img: 'https://firebasestorage.googleapis.com/v0/b/doco-img.appspot.com/o/image%2Fformal%20pic%204%20(1)%20-%20Shristi%20Singh.png?alt=media&token=d72df669-460e-423c-b8c3-fa7c1f53a878',
+	},
+	{
+		name: 'Rishabh Raj',
+		dept: 'Mentor',
+		pos: 'Web Development',
+		img: 'https://drive.google.com/uc?id=1QBvDzIQWQZZtJLnqEHmwczVV8iyLVzw0',
+	},
+	{
+		name: 'Abhijeet Ranjan',
+		dept: 'Mentor',
+		pos: 'UI/UX Designing',
+		img: 'https://drive.google.com/uc?id=1ghQxgdJw_kl8PoRA4YvAnORCa7S3iMrn',
+	},
+	{
+		name: 'Faizal Ahmed Farooq',
+		dept: 'Mentor',
+		pos: 'Open Source',
+		img: 'https://drive.google.com/uc?id=1xopknwAwmrHjCWYjhYvyNnpF2uqp-7i0',
+	},
+	{
+		name: 'Sanjay Aradhyamath',
+		dept: 'Mentor',
+		pos: 'AI/ML',
+		img: 'https://drive.google.com/uc?id=1GTuE78oB0W5LvenO0Telym1ypMxgwdaH',
+	},
+	{
+		name: 'Gyanesh Singh Sachan',
+		dept: 'Mentor',
+		pos: 'AI/ML',
+		img: 'https://drive.google.com/uc?id=1dNSMv5RGaD4ANG5RYv0LuCTrYyenLiAz',
+	},
+	{
+		name: 'Shikhar Jaiswal',
+		dept: 'Mentor',
+		pos: 'AI/ML',
+		img: 'https://drive.google.com/uc?id=14uk_VmUVKS-9Sj8wA8R1orBQ8RW13uNz',
+	},
+	{
+		name: 'Chandan H',
+		dept: 'Mentor',
+		pos: 'Cloud Computing',
+		img: 'https://drive.google.com/uc?id=1pSytulz0sYbte-qDSApD3oKGNz9Hn7vx',
+	},
+];
+
+export const categories = Array.from(new Set(members.map((member) => member.dept)));
